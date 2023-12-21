5f58ae3151f1d3fe7c3ac20b0ed97c1026f515ea
Ch3mson <bensonyan778@hotmail.com>
2023-12-21 01:30:51 -0800

    Added card template

--- qr-code/src/App.jsx
+++ qr-code/src/App.jsx
@@ -2,7 +2,10 @@
 function App() {
 
   return (
-    <p className="text-white">hello</p>
+    <div class="flex flex-col justify-center items-center p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Create QR Code:</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Enter the QR code url and click generate.</p>
+    </div>
   )
 }
 
