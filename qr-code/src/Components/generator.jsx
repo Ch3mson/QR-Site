bbe3fd39bb40f0fdf3f39ea24996423e9525de8a
Ch3mson <bensonyan778@hotmail.com>
2023-12-21 17:35:19 -0800

    Final changes

--- qr-code/src/Components/generator.jsx
+++ qr-code/src/Components/generator.jsx
@@ -1,10 +1,50 @@
+import QRCode from 'qrcode';
+import { useState } from 'react';
 
 function Generator () {
+    const [url, seturl] = useState('');
+    const [qrcode, setQrcode] = useState('');
+
+    const GenerateQRCode = () => {
+        QRCode.toDataURL(url, {
+            width: 800,
+            margin: 1.5,
+        }, (err, url) => {
+            if (err) return console.error(err)
+
+            console.log(url)
+            setQrcode(url)
+        })
+    }
     return (
-        <div className="flex p-3 place-content-center">
-            <input type="text" className=" mr-3 py-1 px-2 rounded bg-opacity-20 bg-indigo-500 w-[20rem] text-white leading-tight focus:outline-none" 
-            placeholder="enter url here"></input>
-            <button className=" mr-3 py-1 px-2 rounded outline outline-offset-2 outline-indigo-500 hover:outline-indigo-400 bg-indigo-500 bg-opacity-20 hover:bg-indigo-400 hover:bg-opacity-20">Generate</button>
+        <div className="flex flex-col items-center p-3">
+            <div className="flex justify-center w-full mb-4">
+                <input 
+                    type="text" 
+                    className="mr-3 py-1 px-2 rounded bg-[#25234a] w-[20rem] text-white focus:outline-none focus:ring-2 focus:ring-[#444087]" 
+                    placeholder="Enter URL here"
+                    value={url}
+                    onChange={(evt) => seturl(evt.target.value)}
+                />
+                <button 
+                    onClick={GenerateQRCode}
+                    className="py-1 px-2 rounded outline outline-offset-2 outline-[#2c2958] hover:outline-[#2c2958] bg-[#25234a] hover:bg-[#444087] "
+                >
+                    Generate
+                </button>
+            </div>
+
+            {/* Render QR code only if it has a value */}
+            {qrcode && (
+                <a href={qrcode} download="qrcode.png">
+                    <img 
+                        className="object-contain w-[300px] h-[300px] outline outline-indigo-500 hover:outline-indigo-400 outline-7 mt-5"
+                        src={qrcode}
+                        alt="Generated QR Code"
+                    />
+                </a>
+            )}
+            
         </div>
     )
 }
